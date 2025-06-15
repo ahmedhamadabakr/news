ba2467ec032423f6c3422c8f2645d51e67155814
Ahmed Hamada Bakr <ahmedhamadabakr77@gmail.com>
2025-06-15 17:17:57 +0300

    Update Errorview to enforce type safety by specifying String for textError. Refactor NewsListviewBilder to use a StatefulWidget, initializing future data fetching in initState for improved state management.

--- lib/error/error.dart
+++ lib/error/error.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Errorview extends StatelessWidget {
   const Errorview({super.key, required this.textError});
 
-  final textError;
+  final String textError;
 
   @override
   Widget build(BuildContext context) {
--- lib/widgets/news_list_view_bilder.dart
+++ lib/widgets/news_list_view_bilder.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:news/error/error.dart';
+import 'package:news/model/article_model.dart';
 import 'package:news/services/news_servce.dart';
 import 'package:news/views/loading_view.dart';
 import 'package:news/widgets/tilelist_view.dart';
 
-class NewsListviewBilder extends StatelessWidget {
+class NewsListviewBilder extends StatefulWidget {
   const NewsListviewBilder({super.key});
+
+  @override
+  State<NewsListviewBilder> createState() => _NewsListviewBilderState();
+}
+
+class _NewsListviewBilderState extends State<NewsListviewBilder> {
+  var future;
+  @override
+  void initState() {
+    super.initState();
+    future = NewsServce().getNews();
+  }
 
   @override
   Widget build(BuildContext context) {
     // come with <future>
-    return FutureBuilder(
-      future: NewsServce().getNews(),
+    return FutureBuilder<List<ArticleModel>>(
+      future: future,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           return TilelistViewState(articles: snapshot.data!);
